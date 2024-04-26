136a6de9f8d86d0587fae664162a51231bee0657
bibhushan <paudelbibhushan@gmail.com>
2024-04-26 09:24:50 +0545

    I added a file

---
+++ config/routes.rb
@@ -0,0 +1,18 @@
+Rails.application.routes.draw do
+  devise_for :installs
+  # get "/blog_posts/new" , to: "blog_posts#new", as: :new_blog_post
+  # get "/blog_posts/:id/edit" , to: "blog_posts#edit", as: :edit_blog_post
+  # patch "/blog_posts/:id" , to: "blog_posts#update"
+  # delete "/blog_posts/:id" , to: "blog_posts#destroy"
+  # get "/blog_posts/:id" , to: "blog_posts#show", as: :blog_post
+  # post "/blog_posts" , to: "blog_posts#create", as: :blog_posts
+  
+  resources :blog_posts
+  # defines the root path route("/")
+
+  root "blog_posts#index"
+
+   
+ 
+
+end
