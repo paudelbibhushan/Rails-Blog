b9b6f342885214d56548cb0c716cd8e4297a29c8
bibhushan <paudelbibhushan@gmail.com>
2024-04-30 15:04:17 +0545

    Trying to overcome the mistake

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_26_040048) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_29_014402) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_26_040048) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "published_at"
   end
 
   create_table "installs", force: :cascade do |t|
